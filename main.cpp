c7b94cfc3cba6534c009a3aefa055761e3e0fd53
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-11-14 18:18:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main()
 {
-    int n=0,b=1,a=0,c=0;
+    int n=0,i=0,b=1,a=0,c=0;
     cin>>n;
-    
     cout<<b<<endl;
-    for(c=0;c<n;c++)
+     for(i=0;i<n;i++)
         {
+         
          c=a+b;
          cout<<c<<endl;
          a=b;
          b=c;
-         }
+        }
          
     return 0;
 }
